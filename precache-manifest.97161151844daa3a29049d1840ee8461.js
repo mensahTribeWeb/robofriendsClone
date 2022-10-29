a98faa3e164ee939094683321a95a696a4a455fc
mensahTribeWeb <ndm.acct85@gmail.com>
2022-10-29 00:08:24 -0500

    Updates

---
+++ precache-manifest.97161151844daa3a29049d1840ee8461.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b474a12c15d3c1849a4",
+    "url": "/robofriendsClone/static/css/main.e479ac9c.chunk.css"
+  },
+  {
+    "revision": "7b474a12c15d3c1849a4",
+    "url": "/robofriendsClone/static/js/main.7b474a12.chunk.js"
+  },
+  {
+    "revision": "84cf99745ce0980187aa",
+    "url": "/robofriendsClone/static/js/1.84cf9974.chunk.js"
+  },
+  {
+    "revision": "6da71f451ee4d478f0c2",
+    "url": "/robofriendsClone/static/css/2.ecf7398f.chunk.css"
+  },
+  {
+    "revision": "6da71f451ee4d478f0c2",
+    "url": "/robofriendsClone/static/js/2.6da71f45.chunk.js"
+  },
+  {
+    "revision": "06ce74b5e79feef501bc",
+    "url": "/robofriendsClone/static/js/runtime~main.06ce74b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsClone/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4f7aab2966f5fb40b51a415982fb35fa",
+    "url": "/robofriendsClone/index.html"
+  }
+];
